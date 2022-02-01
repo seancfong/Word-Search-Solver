20a404c2e158bafca07f1d3155efb0635e51db05
Sean Fong <seancfong@gmail.com>
2022-02-01 00:27:43 -0800

    Begin interface

--- src/interface.py
+++ src/interface.py
@@ -2,3 +2,26 @@
 The interface.py module implements functionality for creating
 a user interface to import and view solvable puzzles
 '''
+
+import tkinter
+
+class SolverApp:
+    def __init__(self):
+        self._root_window = tkinter.Tk()
+
+    def _refresh_display(self):
+        '''
+        Refreshes the display
+        '''
+        pass
+
+    def run(self):
+        '''
+        Runs the main loop
+        '''
+        self._refresh_display()
+        self._root_window.mainloop()
+
+    
+if __name__ == '__main__':
+    SolverApp().run()
