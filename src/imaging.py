c6ab3488265eba50bbc263807921933c5e83ab7c
Sean Fong <seancfong@gmail.com>
2022-02-02 21:37:09 -0800

    Add interface and imaging modules

---
+++ src/imaging.py
@@ -0,0 +1,47 @@
+'''
+The imaging.py module implements functionality for reading images
+and extracting textual data using pytesseract and OpenCV
+'''
+import math
+
+import pytesseract
+import cv2
+
+# Configuration for pytesseract
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+class Imaging:
+    def __init__(self, img: str) -> None:
+        # read the image
+        self._image = cv2.imread(img)
+        # convert image to rgb
+        self._image = cv2.cvtColor(self._image, cv2.COLOR_BGR2RGB)
+        # get the height and width of image
+        self._image_height, self._image_width, _ = self._image.shape
+
+    def get_image(self) -> 'Image':
+        return self._image
+
+    def get_image_dimensions(self) -> (int, int):
+        '''
+        Returns the dimensions of the image as a tuple of integers
+        '''
+        return self._image_width, self._image_height
+
+    def resize(self, width, height) -> None:
+        '''
+        Resizes the image
+        '''
+        width = math.ceil(width)
+        height = math.ceil(height)
+        self._image = cv2.resize(self._image, (width, height))
+
+
+
+
+# For testing the module
+if __name__ == '__main__':
+    imager = Imaging('example_1.jpg')
+    cv2.imshow('Test image', imager.get_image())
+    cv2.waitKey(0)
+
--- src/interface.py
+++ src/interface.py
@@ -3,23 +3,70 @@ The interface.py module implements functionality for creating
 a user interface to import and view solvable puzzles
 '''
 
+import imaging
 import tkinter
+import cv2
+from PIL import Image, ImageTk
+from tkinter import filedialog
 
 class SolverApp:
-    def __init__(self):
+    def __init__(self) -> None:
         self._root_window = tkinter.Tk()
+        self._root_window.title('Word Search')
+        self._root_window.geometry('800x600')
+        self._root_window.minsize(width=300, height=300)
+        self._image_object = None
+        self._image_panel = tkinter.Label()
+        self._open_button = tkinter.Button(
+            self._root_window, text='Open Image', command=self._open_image
+        )
+        self._open_button.grid(
+            row=0, column=0
+        )
+        self._image_panel.grid(
+            row=0, column=1,
+            sticky=tkinter.N + tkinter.S + tkinter.E + tkinter.W
+        )
 
-    def _refresh_display(self):
-        '''
-        Refreshes the display
-        '''
-        pass
 
-    def run(self):
+
+    def _convert_image(self) -> 'PhotoImage':
+        '''
+        Converts the cv2 image into a format supported by Tkinter to display
+        '''
+        # convert cv2 image to PIL
+        to_display = Image.fromarray(self._image_object.get_image())
+        # convert PIL image to ImageTk
+        to_display = ImageTk.PhotoImage(to_display)
+        return to_display
+
+
+    def _open_image(self) -> None:
+        '''
+        Prompts the user to select and image and
+        displays it in the interface
+        '''
+        # prompt user to open an image
+        path = filedialog.askopenfilename()
+        print(path)
+
+        # update current image
+        self._image_object = imaging.Imaging(path)
+        img_width, img_height = self._image_object.get_image_dimensions()
+        window_height = self._root_window.winfo_screenheight()
+        print(window_height * img_width / img_height, img_height)
+        self._image_object.resize(window_height * img_width / img_height, img_height)  # TODO: fix math
+        to_display = self._convert_image()
+
+        self._image_panel.configure(image=to_display)
+        # set an attribute to the label in order for the image to render correctly
+        self._image_panel.image = to_display
+
+
+    def run(self) -> None:
         '''
         Runs the main loop
         '''
-        self._refresh_display()
         self._root_window.mainloop()
 
 
