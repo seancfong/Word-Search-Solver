bd22a74f38ed66330bb3eb0dfe20408d723374f7
Sean Fong <seancfong@gmail.com>
2022-02-03 23:52:14 -0800

    Fix image resizing

--- src/interface.py
+++ src/interface.py
@@ -4,6 +4,7 @@ a user interface to import and view solvable puzzles
 '''
 
 import imaging
+import math
 import tkinter
 import cv2
 from PIL import Image, ImageTk
@@ -15,6 +16,7 @@ class SolverApp:
         self._root_window.title('Word Search')
         self._root_window.geometry('800x600')
         self._root_window.minsize(width=300, height=300)
+        self._root_window.bind('<Configure>', image_resizer(self))
         self._image_object = None
         self._image_panel = tkinter.Label()
         self._open_button = tkinter.Button(
@@ -28,14 +30,15 @@ class SolverApp:
             sticky=tkinter.N + tkinter.S + tkinter.E + tkinter.W
         )
 
-
-
     def _convert_image(self) -> 'PhotoImage':
         '''
         Converts the cv2 image into a format supported by Tkinter to display
         '''
         # convert cv2 image to PIL
         to_display = Image.fromarray(self._image_object.get_image())
+        img_width, img_height = map(math.ceil, self._image_object.get_image_dimensions())
+        window_height = self._root_window.winfo_height()
+        to_display = to_display.resize((window_height * img_width // img_height, window_height))
         # convert PIL image to ImageTk
         to_display = ImageTk.PhotoImage(to_display)
         return to_display
@@ -48,19 +51,9 @@ class SolverApp:
         '''
         # prompt user to open an image
         path = filedialog.askopenfilename()
-        print(path)
-
         # update current image
         self._image_object = imaging.Imaging(path)
-        img_width, img_height = self._image_object.get_image_dimensions()
-        window_height = self._root_window.winfo_screenheight()
-        print(window_height * img_width / img_height, img_height)
-        self._image_object.resize(window_height * img_width / img_height, img_height)  # TODO: fix math
-        to_display = self._convert_image()
-
-        self._image_panel.configure(image=to_display)
-        # set an attribute to the label in order for the image to render correctly
-        self._image_panel.image = to_display
+        image_resizer(self)()
 
 
     def run(self) -> None:
@@ -68,6 +61,22 @@ class SolverApp:
         Runs the main loop
         '''
         self._root_window.mainloop()
+
+
+def image_resizer(self):
+    def _update_image(*event) -> None:
+        '''
+        Updates the current image displayed
+        '''
+        # update the image
+        to_display = self._convert_image()
+
+        # update the image panel
+        self._image_panel.configure(image=to_display)
+        # set an attribute to the label in order for the image to render correctly
+        self._image_panel.image = to_display
+
+    return _update_image
 
 
 if __name__ == '__main__':
